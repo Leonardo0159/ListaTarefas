8680afcf3f11e2f7c8faa2da59c64b57d7904d68
unknown <leo.sn159@gmail.com>
2022-02-10 17:09:12 -0300

    List Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import * as C from './App.styles'
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem'
 
 const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "teste 1", done: false},
+    { id: 2, name: "teste 2", done: false}
+  ]);
+
   return (
     <C.Container>
-      
+      <C.Area>
+        <C.Header>
+          Lista de Tarefas
+        </C.Header>
+
+        {/* adicionar nova tarefa*/}
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item}/>
+        ))}
+
+      </C.Area>
     </C.Container>
   )
 }
